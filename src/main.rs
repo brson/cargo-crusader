76d07995746372344e777fbcdd8ed81078a828ab
Brian Anderson <banderson@mozilla.com>
2015-08-02 14:13:33 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+extern crate env_logger;
+#[macro_use]
+extern crate log;
 extern crate semver;
+extern crate toml;
 
 use semver::Version;
+use std::convert::From;
 use std::env;
+use std::fs::File;
+use std::io::{self, Read};
 use std::path::{PathBuf, Path};
 use std::sync::mpsc::{self, Sender, Receiver};
 
 fn main() {
+    env_logger::init().unwrap();
     report_results(run());
 }
 
@@ -30,8 +38,8 @@ fn run() -> Result<TestResults, Error> {
 
 enum Options {
     Implicit,
-    Base(Rev),
-    Explicit(Rev, Rev)
+    Base(Version),
+    Explicit(Version, Version)
 }
 
 fn parse_options() -> Result<Options, Error> {
@@ -48,26 +56,18 @@ fn parse_options() -> Result<Options, Error> {
     }
 }
 
-fn parse_rev(s: &str) -> Result<Rev, Error> {
-    match Version::parse(s) {
-        Ok(v) => Ok(Rev::Published(v)),
-        Err(_) => {
-            if !s.contains("Cargo.toml") {
-                Err(Error::ManifestName(s.to_string()))
-            } else {
-                Ok(Rev::Source(PathBuf::from(s)))
-            }
-        }
-    }
+fn parse_rev(s: &str) -> Result<Version, Error> {
+    Ok(try!(Version::parse(s)))
 }
 
 struct Config {
+    manifest_path: PathBuf,
     crate_name: String,
-    base_rev: Rev,
-    next_rev: Rev
+    base_origin: Origin,
+    next_origin: Origin
 }
 
-enum Rev {
+enum Origin {
     Published(Version),
     Source(PathBuf)
 }
@@ -75,7 +75,77 @@ enum Rev {
 type VersionNumber = String;
 
 fn get_config(options: Options) -> Result<Config, Error> {
-    unimplemented!()
+    let manifest = env::var("CRUSADER_MANIFEST");
+    let manifest = manifest.unwrap_or_else(|_| "./Cargo.toml".to_string());
+    let manifest = PathBuf::from(manifest);
+
+    let source_name = try!(get_crate_name(&manifest));
+    match options {
+        Options::Implicit => {
+            let rev = try!(get_most_recent_rev(&source_name));
+            Ok(Config {
+                manifest_path: manifest.clone(),
+                crate_name: source_name,
+                base_origin: Origin::Published(rev),
+                next_origin: Origin::Source(manifest)
+            })
+        }
+        Options::Base(base_rev) => {
+            Ok(Config {
+                manifest_path: manifest.clone(),
+                crate_name: source_name,
+                base_origin: Origin::Published(base_rev),
+                next_origin: Origin::Source(manifest)
+            })
+        }
+        Options::Explicit(base_rev, next_rev) => {
+            Ok(Config {
+                manifest_path: manifest.clone(),
+                crate_name: source_name,
+                base_origin: Origin::Published(base_rev),
+                next_origin: Origin::Published(next_rev)
+            })
+        }
+    }
+}
+
+fn get_crate_name(manifest_path: &Path) -> Result<String, Error> {
+    let ref toml = try!(load_string(manifest_path));
+    let mut parser = toml::Parser::new(toml);
+    let toml = parser.parse();
+    let map = if toml.is_none() {
+        return Err(Error::TomlError(parser.errors))
+    } else {
+        toml.unwrap()
+    };
+
+    match map.get("package") {
+        Some(&toml::Value::Table(ref t)) => {
+            match t.get("name") {
+                Some(&toml::Value::String(ref s)) => {
+                    Ok(s.clone())
+                }
+                _ => {
+                    Err(Error::ManifestName(PathBuf::from(manifest_path)))
+                }
+            }
+        }
+        _ => {
+            Err(Error::ManifestName(PathBuf::from(manifest_path)))
+        }
+    }
+}
+
+fn load_string(path: &Path) -> Result<String, Error> {
+    let mut file = try!(File::open(path));
+    let mut s = String::new();
+    try!(file.read_to_string(&mut s));
+    Ok(s)
+}
+
+fn get_most_recent_rev(crate_name: &str) -> Result<Version, Error> {
+    // TODO
+    Ok(try!(Version::parse("0.0.0")))
 }
 
 struct RevDep;
@@ -99,6 +169,7 @@ impl Drop for Crates {
     }
 }
 
+#[derive(Debug)]
 struct TestResults;
 
 impl TestResults {
@@ -115,12 +186,14 @@ impl TestResults {
     }
 }
 
+#[derive(Debug)]
 enum TestResult {
     Broken(CompileResult),
     Fail(CompileResult, CompileResult),
     Pass(CompileResult, CompileResult)
 }
 
+#[derive(Debug)]
 struct TestResultFuture;
 
 fn new_result_future() -> (Sender<TestResult>, TestResultFuture) {
@@ -157,6 +230,7 @@ fn run_test_local(base_crate: &Path, next_crate: &Path, ref rev_dep: RevDep) -> 
     }
 }
 
+#[derive(Debug)]
 struct CompileResult {
     stdout: String,
     stderr: String,
@@ -172,10 +246,27 @@ fn compile_with_custom_dep(rev_dep: &RevDep, krate: &Path) -> CompileResult {
 }
 
 fn report_results(res: Result<TestResults, Error>) {
+    println!("results: {:?}", res);
 }
 
+#[derive(Debug)]
 enum Error {
     BadArgs,
-    ManifestName(String),
+    ManifestName(PathBuf),
     TestFailure(TestResults),
+    SemverError(semver::ParseError),
+    TomlError(Vec<toml::ParserError>),
+    IoError(io::Error),
+}
+
+impl From<semver::ParseError> for Error {
+    fn from(e: semver::ParseError) -> Error {
+        Error::SemverError(e)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error::IoError(e)
+    }
 }
